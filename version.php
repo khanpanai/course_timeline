899a217b3f3f84bb8582a6cd2d16885aedbd9ab5
hanpanai <work@verrok.ru>
2024-01-06 15:07:36 +0300

    chore: bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_course_timeline';
-$plugin->release = '0.4.0';
+$plugin->release = '0.5.0';
 $plugin->version = 2023121200;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
